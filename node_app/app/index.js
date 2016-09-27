bf1faa362c3a1fe66eb2cf89bfdcde0504be3ca1
Ashish Ranjan <asr@one.com>
2016-09-27 16:22:20 +0200

    adding fluent-logger in the node app

--- node_app/app/index.js
+++ node_app/app/index.js
@@ -2,11 +2,22 @@
 import express from 'express';
 const app = express();
 const port = 8080
-
+var logger = require('fluent-logger');
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 var server = app.listen(port, () => {
   console.log(`Example app listening at http:\/\/localhost:${port}`);
+  var logger = require('fluent-logger')
+// The 2nd argument can be omitted. Here is a default value for options.
+logger.configure('docker', {
+   host: 'fluentd',
+   port: 24224,
+   timeout: 3.0,
+   reconnectInterval: 600000 // 10 minutes
+});
+setInterval(()=>{
+  //logger.emit('appEvent', {id:bidId, taskId:taskId, action:'new', type:'bid'})
+},400)
 });
